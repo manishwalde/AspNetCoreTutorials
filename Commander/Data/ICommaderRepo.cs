2502d7201560eff05629710305a712d3b133fef0
mwalde <mwalde@shr.global>
2020-09-24 19:29:18 +0530

    Commander project commit

---
+++ Commander/Data/ICommaderRepo.cs
@@ -0,0 +1,16 @@
+using System.Collections.Generic;
+using Commander.Models;
+
+namespace Commander.Data
+{
+    public interface ICommaderRepo
+    {
+        bool SaveChanges();
+        
+        IEnumerable<Command> GetAllCommands();
+        Command GetCommandById(int id);
+        void CreateCommand(Command command);
+        void UpdateCommand(Command command);
+        void DeleteCommand(Command command);
+    }
+}
